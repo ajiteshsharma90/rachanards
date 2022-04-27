ddc6c8a5fbebb60c56cb141291a0e359c8aeeb4b
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 21:59:51 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -10,15 +10,15 @@ locals {
 }
 
 
-resource "aws_db_instance" "default1" {
+resource "aws_db_instance" "default" {
 allocated_storage = 20
 identifier = "testinstance2"
 storage_type = "gp2"
-engine = "mysql"
-engine_version = "5.7"
-instance_class = "db.m4.large"
+engine = "sqlserver-ex"
+engine_version = "14.00.3401.1.v1"
+instance_class = "db.r5.xlarge"
 name = "test4"
 username = local.db_creds.username
 password = local.db_creds.password
-parameter_group_name = "default.mysql5.7"
+parameter_group_name = "default.sqlserver-ex14.0"
 }
