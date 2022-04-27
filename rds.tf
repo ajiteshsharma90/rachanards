bc9524bb8185ccdaab51fbe1193df62858fd523a
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 02:21:19 -0400

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,24 @@
+data "aws_secretsmanager_secret_version" "creds" {
+  # Fill in the name you gave to your secret
+  secret_id = "db-creds"
+}
+
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
+
+resource "aws_db_instance" "default" {
+allocated_storage = 20
+identifier = "testinstance2"
+storage_type = "gp2"
+engine = "mysql"
+engine_version = "5.7"
+instance_class = "db.m4.large"
+name = "test4"
+username = local.db_creds.username
+password = local.db_creds.password
+parameter_group_name = "default.mysql5.7"
+}
