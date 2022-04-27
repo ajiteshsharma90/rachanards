dae25bd4df8b19ebaa4bfd9e6113f83f74bdef3c
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 02:12:28 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+    access_key = ${{DEV_AWS_ACCESS_KEY_ID}}
+    secret_key = ${{DEV_AWS_SECRET_ACCESS_KEY}}
+    #access_key = local.aws_creds.AWS_ACCESS_KEY
+    #secret_key = local.aws_creds.AWS_SECRET_KEY
+    region = "us-west-2"
+}
