ad58c8ccdcc047f4eb5524e621f5a1a28c79bba8
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 02:25:53 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = $"{{DEV_AWS_ACCESS_KEY_ID}}"
-    secret_key = $"{{DEV_AWS_SECRET_ACCESS_KEY}}"
+    access_key = "AKIASJAGHY2P5MM6BR7C"
+    secret_key = "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"
     #access_key = local.aws_creds.AWS_ACCESS_KEY
     #secret_key = local.aws_creds.AWS_SECRET_KEY
     region = var.aws_region
